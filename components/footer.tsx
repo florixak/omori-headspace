642d9bd2a8f2f79b13f598acf02ab693c2c0ca54
ptakondrej <florixakcz@seznam.cz>
2025-11-09 21:29:48 +0100

    Update footer styling and content for Headspace
    Refactored footer to improve styling and accessibility, including dynamic color changes based on space, updated text content, and added aria-labels to social links. The creator's name now links to GitHub with conditional styling, and minor text corrections were made for clarity.

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,6 +7,7 @@ import Link from "next/link";
 
 const Footer = () => {
   const { space } = useSpaceStore();
+  const isHeadspace = space === "headspace";
   return (
     <footer className="flex flex-col w-full border-t-4 border-(--omori-black) px-10 py-12 gap-12">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-between space-y-8">
@@ -19,17 +20,21 @@ const Footer = () => {
               height={50}
             />
 
-            <span className="text-xl pixel-text">OMORI FAN SITE</span>
+            <span className="text-xl pixel-text">OMORI HEADSPACE</span>
           </div>
           <p className="text-sm max-w-sm">
-            A fan-made tribute to OMORI Everything is going to be okay.
+            A fan-made tribute to OMORI. Everything is going to be okay.
           </p>
         </div>
         <div className="flex flex-col space-y-2">
           <h4 className="text-sm uppercase tracking-wide font-mono">EXPLORE</h4>
           <ul className="flex flex-col space-y-1">
             {navLinks.map((link) => (
-              <Link key={link.label} href={link.href}>
+              <Link
+                key={link.label}
+                href={link.href}
+                className="text-sm hover:underline transition-all duration-200"
+              >
                 {link.label}
               </Link>
             ))}
@@ -38,21 +43,20 @@ const Footer = () => {
         <div className="flex flex-col space-y-2">
           <h4 className="text-sm uppercase tracking-wide font-mono">CONNECT</h4>
           <div className="flex flex-row flex-wrap gap-2">
-            <div className="flex flex-row flex-wrap gap-2">
-              {socials.map((social) => (
-                <Link key={social.label} href={social.href}>
-                  <div
-                    className={`rpg-border p-2 ${
-                      space === "headspace"
-                        ? "hover:bg-(--omori-purple)"
-                        : "hover:bg-(--kel-orange)"
-                    } hover:text-(--omori-white) hover:border-(--omori-black) transition-colors duration-200`}
-                  >
-                    <social.icon />
-                  </div>
-                </Link>
-              ))}
-            </div>
+            {socials.map((social) => (
+              <Link key={social.label} href={social.href}>
+                <div
+                  aria-label={social.label}
+                  className={`rpg-border p-2 ${
+                    space === "headspace"
+                      ? "hover:bg-(--omori-purple)"
+                      : "hover:bg-(--kel-orange)"
+                  } hover:text-(--omori-white) hover:border-(--omori-black) transition-colors duration-200`}
+                >
+                  <social.icon />
+                </div>
+              </Link>
+            ))}
           </div>
           <p className="text-sm max-w-sm">
             Join the community and share your love for OMORI
@@ -63,7 +67,18 @@ const Footer = () => {
       <div className="flex flex-col gap-4">
         <div className="flex flex-col md:flex-row items-center justify-between w-full gap-2">
           <p className="text-sm text-center md:text-left text-wrap">
-            © 2025 OMORI Headspace Made With 💜 By Ondřej Pták
+            © 2025 OMORI Headspace Made With 💜 By{" "}
+            <a
+              href="https://www.github.com/florixak"
+              target="_blank"
+              className={
+                isHeadspace
+                  ? "text-(--omori-purple) hover:underline"
+                  : "text-(--kel-orange) hover:underline"
+              }
+            >
+              Ondřej Pták
+            </a>
           </p>
           <div className="flex items-center gap-2 px-4 py-2 bg-(--omori-black) text-(--omori-white) text-sm">
             <span>NOT OFFICIAL</span>
@@ -74,8 +89,8 @@ const Footer = () => {
         <div className="battle-box text-center p-6">
           <p className="text-sm max-w-3xl mx-auto">
             This is a fan-made website. OMORI and all related characters are
-            property of OMOCAT LLC. <br /> All content is used for
-            non-commercial fan purposes only.
+            property of OMOCAT LLC. <br /> All the content is owned by OMOCAT
+            LLC and it is used for non-commercial fan purposes only.
           </p>
         </div>
       </div>
