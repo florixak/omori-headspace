bb85171508c2563e78bd873a39bae60dc24d164a
ptakondrej <florixakcz@seznam.cz>
2025-12-10 19:38:03 +0100

    Refactor mobile navigation into separate component
    Extracted mobile navigation logic from Header into a new MobileNavigation component for improved modularity and maintainability. Added GSAP-based animation for mobile menu transitions.

---
+++ components/mobile-navigation.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { navLinks } from "@/constants";
+import NavLink from "./nav-link";
+import { useEffect, useEffectEvent, useRef, useState } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+interface MobileNavigationProps {
+  isOpen: boolean;
+  toggleMenu: () => void;
+}
+
+const MobileNavigation = ({ isOpen, toggleMenu }: MobileNavigationProps) => {
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  const [shouldRender, setShouldRender] = useState(isOpen);
+
+  const effectFn = useEffectEvent(() => {
+    if (isOpen) {
+      setShouldRender(true);
+    }
+  });
+
+  useEffect(() => {
+    effectFn();
+  }, [isOpen]);
+
+  useGSAP(() => {
+    if (!menuRef.current) return;
+
+    if (isOpen) {
+      gsap.fromTo(
+        menuRef.current,
+        { y: -50, opacity: 0, pointerEvents: "none" },
+        {
+          y: 0,
+          opacity: 1,
+          pointerEvents: "auto",
+          duration: 0.3,
+          ease: "power2.out",
+        }
+      );
+    } else {
+      gsap.to(menuRef.current, {
+        y: -50,
+        opacity: 0,
+        pointerEvents: "none",
+        duration: 0.3,
+        ease: "power2.in",
+        onComplete: () => setShouldRender(false),
+      });
+    }
+  }, [isOpen, shouldRender]);
+
+  if (!shouldRender) return null;
+
+  return (
+    <div
+      ref={menuRef}
+      className="md:hidden fixed top-22 left-0 right-0 w-full bg-(--omori-white) z-49"
+    >
+      <ul className="flex flex-col items-center gap-1 py-4">
+        {navLinks.map((link) => (
+          <NavLink
+            key={link.href}
+            label={link.label}
+            href={link.href}
+            onClick={toggleMenu}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default MobileNavigation;
