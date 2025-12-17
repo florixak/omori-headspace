162a2f2253f149e79ebbf7d062d1e6c4408ce246
ptakondrej <florixakcz@seznam.cz>
2025-12-17 08:42:22 +0100

    Update component import paths in page.tsx
    Adjusted import statements to reference component subdirectories explicitly, improving clarity and consistency in the file structure.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import Characters from "@/components/characters";
-import Emotions from "@/components/emotions";
+import Characters from "@/components/characters/characters";
+import Emotions from "@/components/emotions/emotions";
 import Footer from "@/components/footer";
-import Gallery from "@/components/gallery";
+import Gallery from "@/components/gallery/gallery";
 import Header from "@/components/header";
 import Hero from "@/components/hero";
-import Locations from "@/components/locations";
-import Quotes from "@/components/quotes";
+import Locations from "@/components/locations/locations";
+import Quotes from "@/components/quotes/quotes";
 
 export default function Home() {
   return (
