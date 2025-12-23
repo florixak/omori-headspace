62d9699771505ff88869a47a9ad78e34c3338dfb
ptakondrej <florixakcz@seznam.cz>
2025-12-23 21:24:19 +0100

    Move nav link styles to Link component
    Shifted styling classes from the <li> to the <Link> element in NavLink for improved styling control and consistency.

--- components/nav-link.tsx
+++ components/nav-link.tsx
@@ -12,14 +12,16 @@ interface NavLinkProps {
 const NavLink = ({ label, href, onClick }: NavLinkProps) => {
   const { space } = useSpaceStore();
   return (
-    <li
-      className={`px-4 py-2 text-sm border-2 border-transparent ${
-        space === "headspace"
-          ? "hover:bg-(--omori-purple)"
-          : "hover:bg-(--kel-orange)"
-      } hover:border-(--omori-black) transition-colors duration-200`}
-    >
-      <Link href={href} onClick={onClick}>
+    <li>
+      <Link
+        href={href}
+        onClick={onClick}
+        className={`px-4 py-3 text-sm border-2 border-transparent ${
+          space === "headspace"
+            ? "hover:bg-(--omori-purple)"
+            : "hover:bg-(--kel-orange)"
+        } hover:border-(--omori-black) transition-colors duration-200`}
+      >
         {label}
       </Link>
     </li>
